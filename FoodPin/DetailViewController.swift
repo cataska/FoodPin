f9d8f4c802fa062c6afeff9bc28219694d2add48
Wen-Chun Lin <lovecankill@gmail.com>
2015-07-07 00:14:39 +0800

    * Add missing self sizing cells
    Add toolbar and bar button

--- FoodPin/DetailViewController.swift
+++ FoodPin/DetailViewController.swift
@@ -24,6 +24,9 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
         self.tableView.separatorColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 0.8)
         
         self.restaurantImageView.image = UIImage(named: restaurant.image)
+        
+        self.tableView.estimatedRowHeight = 36.0
+        self.tableView.rowHeight = UITableViewAutomaticDimension
     }
 
     override func didReceiveMemoryWarning() {
