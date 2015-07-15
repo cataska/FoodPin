50563a646fab779e2130c17a1699143e4e30fa4f
Wen-Chun Lin <lovecankill@gmail.com>
2015-07-15 17:26:05 +0800

    Add about page

---
+++ FoodPin/WebViewController.swift
@@ -0,0 +1,40 @@
+//
+//  WebViewController.swift
+//  FoodPin
+//
+//  Created by Lin Wen Chun on 2015/7/15.
+//  Copyright (c) 2015年 Lin Wen Chun. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let url = NSURL(string: "http://www.appcoda.com") {
+            let request = NSURLRequest(URL: url)
+            webView.loadRequest(request)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
