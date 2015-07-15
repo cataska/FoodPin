d2c082191a46d495082228f17712c1fa5bec7328
Wen-Chun Lin <lovecankill@gmail.com>
2015-07-15 19:17:35 +0800

    Add functionality for senting email

--- FoodPin/AboutViewController.swift
+++ FoodPin/AboutViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class AboutViewController: UIViewController {
+class AboutViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,39 @@ class AboutViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func sendEmail(sender: AnyObject) {
+        if MFMailComposeViewController.canSendMail() {
+            let composer = MFMailComposeViewController()
+            composer.mailComposeDelegate = self
+            composer.setToRecipients(["support@appcoda.com"])
+            composer.navigationBar.tintColor = UIColor.whiteColor()
+            presentViewController(composer, animated: true, completion: {
+                UIApplication.sharedApplication().setStatusBarStyle(.LightContent, animated: false)
+            })
+        }
+    }
+    
+    func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
+        
+        switch result.value {
+        case MFMailComposeResultCancelled.value:
+            println("Mail cancelled")
+            
+        case MFMailComposeResultSaved.value:
+            println("Mail saved")
+            
+        case MFMailComposeResultSent.value:
+            println("Mail sent")
+            
+        case MFMailComposeResultFailed.value:
+            println("Failed to send mail: \(error.localizedDescription)")
+            
+        default:
+            break
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
