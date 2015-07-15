a69fb7256d5b2181dcfd3ab2a48dd8fd0a11c869
Wen-Chun Lin <lovecankill@gmail.com>
2015-07-15 21:05:36 +0800

    Add CloudKit support

---
+++ FoodPin/FeedTableViewController.swift
@@ -0,0 +1,116 @@
+//
+//  FeedTableViewController.swift
+//  FoodPin
+//
+//  Created by Lin Wen Chun on 2015/7/15.
+//  Copyright (c) 2015年 Lin Wen Chun. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class FeedTableViewController: UITableViewController {
+    
+    var restaurants: [CKRecord] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.getRecordsFromCloud()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func getRecordsFromCloud() {
+        let cloudContainer = CKContainer.defaultContainer()
+        let publicDatabase = cloudContainer.publicCloudDatabase
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: "Restaurant", predicate: predicate)
+        publicDatabase.performQuery(query, inZoneWithID: nil) { (results, error) -> Void in
+            if error == nil {
+                println("Completed the download of Restaurant data")
+                self.restaurants = results as! [CKRecord]
+                self.tableView.reloadData()
+            } else {
+                println(error)
+            }
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return restaurants.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+
+        // Configure the cell...
+        let restaurant = restaurants[indexPath.row]
+        cell.textLabel?.text = restaurant.objectForKey("name") as? String
+        if restaurant.objectForKey("image") != nil {
+            let imageAsset = restaurant.objectForKey("image") as! CKAsset
+            cell.imageView?.image = UIImage(data: NSData(contentsOfURL: imageAsset.fileURL)!)
+        }
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
